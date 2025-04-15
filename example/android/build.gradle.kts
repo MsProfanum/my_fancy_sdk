30141caf22dea37ab93648155e525a71ad0fd137
Katarzyna Strzyż <msprofanum@gmail.com>
2025-04-15 12:11:20 +0200

    Change the agp version

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath(libs.kotlin.gradlePlugin)
+        classpath(libs.ksp.gradlePlugin)
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -17,12 +28,13 @@ subprojects {
 }
 
 plugins {
-    id("com.android.application").version("8.7.2").apply(false)
-    id("com.android.library").version("8.7.2").apply(false)
+    id("com.android.application").version("8.8.0").apply(false)
+    id("com.android.library").version("8.8.0").apply(false)
     id("androidx.privacysandbox.library").version("1.0.0-alpha02").apply(false)
     id("org.jetbrains.kotlin.jvm").version("1.9.10").apply(false)
     id("com.google.devtools.ksp").version("1.9.10-1.0.13").apply(false)
     id("org.jetbrains.kotlin.plugin.compose").version("2.1.0").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.10").apply(false)
 }
 
 tasks.register<Delete>("clean") {
